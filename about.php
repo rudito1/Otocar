69ea8fe0a6ec4faef6434aa50b842eeefd6b2a3f
ALVIN RAHMAT PRATAMA <79823574+alvinrpra@users.noreply.github.com>
2021-04-27 16:58:57 +0700

    Update about.php

--- about.php
+++ about.php
@@ -28,7 +28,7 @@
 
 <body class="full-wrapper">
     <!-- ? Preloader Start -->
-    <div id="preloader-active">
+<!--     <div id="preloader-active">
         <div class="preloader d-flex align-items-center justify-content-center">
             <div class="preloader-inner position-relative">
                 <div class="preloader-circle"></div>
@@ -37,7 +37,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
     <!-- Preloader Start -->
     <header>
         <!-- Header Start -->
